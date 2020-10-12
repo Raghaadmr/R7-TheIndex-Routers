0b427a8df24772b4a973b793a4971c0669636ff3
Raghad Alrasheed <raghad@MacBook-Pro-alkhas-b-Raghad.local>
2020-10-12 08:54:34 +0300

    fixed yarn

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,20 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 
 // Logo
 import logo from "./assets/theindex.svg";
 
-const Sidebar = () => {
-  return (
-    <div id="sidebar">
-      <img src={logo} className="logo" alt="the index logo" />
-      <section>
-        <h4 className="menu-item">
-          <NavLink to="/authors">AUTHORS</NavLink>
-        </h4>
-      </section>
-    </div>
-  );
-};
+class Sidebar extends Component {
+  render() {
+    return (
+      <div id="sidebar">
+        <img src={logo} className="logo" alt="the index logo" />
+        <section>
+          <h4 className="menu-item">
+            <NavLink to="/authors">AUTHORS</NavLink>
+          </h4>
+          <h4 className="menu-item">
+            <NavLink to="/books">BOOKS</NavLink>
+          </h4>
+        </section>
+      </div>
+    );
+  }
+}
 
 export default Sidebar;
